566bb9f2689b3a53ef3cb491a24e4d63cf3ce9cb
brian fox <briangregoryfox@gmail.com>
2022-02-08 17:22:48 -0800

    first commit

---
+++ fundamentals/fundamentals/for_loop_basic_i.py
@@ -0,0 +1,24 @@
+for x in range(151):
+    print(x)
+
+for y in range(5,1005,5):
+    print(y)
+
+for z in range(1,101,1):
+    if z == 5:
+        print("coding")
+    elif z == 10:
+        print("coding dojo")
+    else:
+        print(z)
+
+for a in range(2018,0,-4):
+    print(a)
+
+lowNum=2
+highNum=9
+mult=3
+
+for b in range(lowNum,highNum,mult):
+    if b % 3:
+        print(b)
